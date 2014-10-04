b58834b371503125d4a84cc062bf3043be2a49cd
Gustav Bylund <maistho@gmail.com>
2014-10-04 20:18:22 +0200

    Added endpoints with input to account-rest

--- account-rest/src/java/se/liu/ida/tdp024/account/rest/service/AccountService.java
+++ account-rest/src/java/se/liu/ida/tdp024/account/rest/service/AccountService.java
@@ -1,7 +1,62 @@
 package se.liu.ida.tdp024.account.rest.service;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+import se.liu.ida.tdp024.account.data.api.entity.Account;
+import se.liu.ida.tdp024.account.data.impl.db.facade.AccountEntityFacadeDB;
+
+@Path("account")
 public class AccountService {
 
+    @GET
+    @Path("/")
+    public Response root( )
+    {
+        return Response.ok().entity("Hello World!").build();
+    }
+  
+    @GET
+    @Path("/create")
+    public Response create(
+    @QueryParam("accounttype") String accounttype,
+    @QueryParam("name") String name,
+    @QueryParam("bank") String bank
+            ) {
+        return Response.ok().entity("accounttype is "
+        + accounttype
+        + "\r\n name is"
+        + name
+        + "\r\n bank is"
+        + bank).build();
+    }    
+    @GET
+    @Path("/find/{name : [\\w(%20)]+}")
+    public Response find( @PathParam("name") String name)
+    {
+        return Response.ok().entity("Hello " + name).build();
+    }    
+    @GET
+    @Path("/credit")
+    public Response credit( )
+    {
+        return Response.ok().entity("Hello World!").build();
+    }
     
+    @GET
+    @Path("/debit")
+    public Response debit( )
+    {
+        return Response.ok().entity("Hello World!").build();
+    }
+    
+    @GET
+    @Path("/transactions")
+    public Response transactions( )
+    {
+        return Response.ok().entity("Hello World!").build();
+    }    
     
 }

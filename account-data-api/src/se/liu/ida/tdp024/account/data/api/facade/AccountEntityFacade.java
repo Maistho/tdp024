9cb83f59b2485518fd162de00c2af3358569bf4d
Filip Marko <zeengai@gmail.com>
2014-10-05 19:46:13 +0200

    Modified exception handling

--- account-data-api/src/se/liu/ida/tdp024/account/data/api/facade/AccountEntityFacade.java
+++ account-data-api/src/se/liu/ida/tdp024/account/data/api/facade/AccountEntityFacade.java
@@ -6,9 +6,14 @@ import se.liu.ida.tdp024.account.data.api.entity.Account;
 
 public interface AccountEntityFacade {
     
+    public class AccountEntityFacadeIllegalArgumentException extends Exception {
+        public AccountEntityFacadeIllegalArgumentException(String message) {
+            super(message);
+        }
+    }
     public void create(String accounttype, String name, String body)
             throws
-            AccountInputParameterException;
+            AccountEntityFacadeIllegalArgumentException;
     
     public List<Account> findAllByName(String key);
 }
